36aa52c3df04042ca7cf0d7b94bf3552835ecec9
Анатолій Колесник <116895889+uitpm221kab@users.noreply.github.com>
2024-01-22 03:38:53 +0200

    Add project files.

---
+++ GFLApp/Data/GFLAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GFLApp.Models;
+
+namespace GFLApp.Data
+{
+    public class GFLAppContext : DbContext
+    {
+        public GFLAppContext (DbContextOptions<GFLAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GFLApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ GFLApp/Models/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using GFLApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace GFLApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new GFLAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<GFLAppContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                using (StreamReader reader = new StreamReader("C:\\Users\\Anatoliy\\source\\repos\\GFLApp\\GFLApp\\Data\\sample.json"))
+                {
+                    string jsonContent = reader.ReadToEnd();
+                    List<Movie> movies = JsonConvert.DeserializeObject<List<Movie>>(jsonContent);
+
+                    if (!context.Movie.Any())
+                    {                      
+                        context.Movie.AddRange(movies);
+                        context.SaveChanges();
+                    }
+                }
+ 
+            }
+        }
+    }
+}
